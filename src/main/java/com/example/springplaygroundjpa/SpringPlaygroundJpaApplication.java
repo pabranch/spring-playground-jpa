3c7bf328335a55ef5eb7ff6648bad14729004013
Perry A Branch <perry.branch@galvanize.com>
2018-09-06 14:05:06 -0400

    Add Spring Boot with Web & DevTools

---
+++ src/main/java/com/example/springplaygroundjpa/SpringPlaygroundJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.springplaygroundjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundJpaApplication.class, args);
+	}
+}
